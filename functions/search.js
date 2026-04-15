7b18f74a86f07d35d624e3ac702bb3b682c06740
indrajitmb21 <indrajitmb21@gmail.com>
2026-04-16 02:33:24 +0530

    Update search.js

--- functions/search.js
+++ functions/search.js
@@ -1,12 +1,28 @@
 export async function onRequest() {
-  return new Response(JSON.stringify([
-    { symbol: "RELIANCE", name: "Reliance Industries Ltd", exchange: "NSE" },
-    { symbol: "TCS", name: "Tata Consultancy Services Ltd", exchange: "NSE" },
-    { symbol: "INFY", name: "Infosys Ltd", exchange: "NSE" }
-  ]), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
+  const API_BASE = "http://nse-api-khaki.vercel.app:5000";
+
+  try {
+    const res = await fetch(`${API_BASE}/symbols`);
+    const data = await res.json();
+
+    const list = Array.isArray(data?.results)
+      ? data.results
+      : Array.isArray(data)
+      ? data
+      : [];
+
+    return new Response(JSON.stringify(list), {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  } catch (err) {
+    return new Response(JSON.stringify([]), {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  }
 }
