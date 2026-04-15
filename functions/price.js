ecfe2534070450e9bebb71a15e1e991c08dc33e9
indrajitmb21 <indrajitmb21@gmail.com>
2026-04-16 01:24:33 +0530

    Update price.js

--- functions/price.js
+++ functions/price.js
@@ -1,6 +1,5 @@
 export async function onRequest(context) {
-  const { request } = context;
-  const url = new URL(request.url);
+  const url = new URL(context.request.url);
   const symbol = (url.searchParams.get("symbol") || "").trim().toUpperCase();
   const exchange = (url.searchParams.get("exchange") || "NSE").trim().toUpperCase();
 
